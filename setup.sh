3c03e9619ced8998134f2d259983a57dd625e6b8
atashandroid <73844924+atashandroid@users.noreply.github.com>
2021-07-19 02:52:20 +0500

    setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
 
+export DATABASE_URL='postgresql://postgres:12345@localhost:5432/music'
+export AUTH0_DOMAIN = 'atash.us.auth0.com
+export AUTH0_CLIENT_ID = 'FJMEFhDBLts8FZIswz6NSuHPerc7DhRu'
+export ALGORITHMS="RS256"
+export API_AUDIENCE="casting"
+export FLASK_APP=app
+export FLASK_DEBUG=True
